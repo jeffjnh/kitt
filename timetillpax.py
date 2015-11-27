dee727da56c42ffeb8eb667096a2fe50221d2d40
Jeff Johansen <jeffjnh@gmail.com>
2015-11-27 17:19:19 +0000

    Tell you the time left till east

--- timetillpax.py
+++ timetillpax.py
@@ -1,31 +1,12 @@
 import time
 import datetime
 from datetime import timedelta,tzinfo
-
-class Zone(tzinfo):
-    def __init__(self,offset,isdst,name):
-        self.offset = offset
-        self.isdst = isdst
-        self.name = name
-    def utcoffset(self, dt):
-        return timedelta(hours=self.offset) + self.dst(dt)
-    def dst(self, dt):
-            return timedelta(hours=1) if self.isdst else timedelta(0)
-    def tzname(self,dt):
-         return self.name
-            
-            
-EST = Zone(-5,False, 'EST')
+from pytz import timezone
 
 
-now = datetime.datetime.now(EST).strftime('%m/%d/%Y %H:%M:%S')
+chosen_time = datetime.datetime.strptime('4/22/2016 10:00:00', '%m/%d/%Y %H:%M:%S')
+chosen_time = chosen_time.replace(tzinfo=timezone('US/Eastern'))
 
-pax = datetime.datetime.strptime('4/22/2015 10:00:00', '%m/%d/%Y %H:%M:%S')
-
-difference = pax - now
-
-
-print (difference)
-
-
-
+delta = (chosen_time - datetime.datetime.now(timezone('US/Eastern')))
+deltastr = str(delta).replace(':', ' hours, ', 1).replace(':', ' minutes, ', 1) + ' seconds!'
+print(deltastr)
